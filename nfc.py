dd22add78eb03f2f9273e0170fa78f68a8af50d9
Dan Lavoie <dan.r.lavoie+github@gmail.com>
2024-03-30 05:25:56 -0400

    Caught a missing dependency and added some comments

--- nfc.py
+++ nfc.py
@@ -2,7 +2,6 @@ import os
 
 import pathlib
 from dotenv import load_dotenv
-from PIL import Image
 import cv2
 
 from kctypes import Direction
@@ -15,6 +14,7 @@ from kctypes import Direction
 # If no id is provided in the init, this is like starting the camera without
 # an NFC card - it sets up a default pics directory.
 class NFC():
+    # id is the unique identifier present with a given NFC card.
     @property
     def id(self):
         return self._id
@@ -22,6 +22,7 @@ class NFC():
     def id(self, value):
         self._id = value
 
+    # pick path is the filepath for pictures for the current NFC card (or default)
     @property
     def pic_path(self):
         return self._pic_path
@@ -29,7 +30,7 @@ class NFC():
     def pic_path(self, value):
         self._pic_path = value
 
-    # Store the last position for each gallery or else use 0
+    # Store the current position for each gallery or else use 0
     @property
     def position(self):
         return self._position
@@ -40,6 +41,11 @@ class NFC():
         self.activate_id(id)
 
     def maybe_create_picture_directory(self):
+        """
+        maybe_create_picture_directory will set up, if it doesn't exist yet,
+        a directory within the base directory to match the current pic path.
+        If it checks and the directory already exists, it does nothing.
+        """
         # Confirm that the base picture directory exists
         if (os.path.exists(self.base_dir)):
             # Then see if a subdir for this nfc id doesn't exist
@@ -51,16 +57,36 @@ class NFC():
             print("NO PIC FOLDER FOUND")
 
     def activate_id(self, id):
+        """
+        activate_id sets up the given NFC card id for picture storage.
+        It sets the given id as the active id, gets a filepath location for
+        storing images, and sets up pointers for wayfinding while viewing
+        files in gallery mode.
+        @param id the id of the NFC card to activate, or the string 'default'
+        """
         self._id = id
         self._pic_path = os.path.join(self.base_dir, self._id)
         self.maybe_create_picture_directory()
-        self.gallery_size = len([name for name in os.listdir(self._pic_path) if os.path.isfile(os.path.join(self._pic_path,name))])
         self._position = 0
 
     def deactivate_id(self):
+        """
+        deactivate_id removes the currently-associated NFC card id from the
+        program state. Functionally this is just activating an NFC card with
+        the id 'default' instead of an actual NFC card id.
+        When no NFC card is connected to the system, teh default directory is
+        used to store gallery files.
+        """
         self.activate_id("default")
 
     def load_image(self):
+        """
+        load_image grabs the filename and extension for the file in the current
+        gallery _pic_path filepath, at the current _position index in the file
+        list, and returns them.
+        The function is called load_image, but it'll load any kind of file, so
+        be careful to only store images or videos in these folders.
+        """
         current_gallery_files = [name for name in os.listdir(self._pic_path) if os.path.isfile(os.path.join(self._pic_path,name))]
         # This will fail if the directory has no pictures in it
         filename = current_gallery_files[self._position]
@@ -71,6 +97,12 @@ class NFC():
         }
 
     def gallery_scroll(self, direction):
+        """
+        gallery_scroll steps forward or backward through the current gallery
+        directory by incrementing or decrementing the _position variable.
+        @param direction a Direction enum, either Direction.FWD or Direction.REV
+        """
+        self.gallery_size = len([name for name in os.listdir(self._pic_path) if os.path.isfile(os.path.join(self._pic_path,name))])
         if direction == Direction.FWD:
             self._position += 1
             if self._position >= self.gallery_size:
@@ -85,26 +117,10 @@ if __name__ == '__main__':
     load_dotenv()
     nfc = NFC(os.getenv('BASE_PIC_PATH'))
     image = nfc.load_image()
-    # Resize the image for display
-    # opencv
     cv2.namedWindow('pic-display', cv2.WINDOW_NORMAL )
     cv2.setWindowProperty('pic-display', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.setWindowProperty('pic-display', cv2.WND_PROP_TOPMOST, 1)
     image = cv2.resize(image, (640, 480))
     cv2.imshow('pic-display', image)
     cv2.waitKey(0)
-    print("GOT HERE")
-    # PIL
-    # image = image.resize((640, 480))
-    # image.show()
-"""
-vid = cv2.VideoCapture(0)
-while(True):
-  ret, frame = vid.read()
-  cv2.imshow('frame', frame)
-  # when 'x' key is pressed the video capture stops
-  if cv2.waitKey(1) & 0xFF == ord('x'):
-    break
-vid.release()
-cv2.destroyAllWindows()
-"""
+

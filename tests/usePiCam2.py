bebc07c9957c2ca48bff20c4cf08b9e95d3a3d5e
Carly <carlyjb@gmail.com>
2024-04-03 16:14:22 -0400

    saving a video

--- tests/usePiCam2.py
+++ tests/usePiCam2.py
@@ -3,11 +3,14 @@ from libcamera import Transform
 import sys
 import time
 import datetime
+import os.path
+from picamera2.encoders import H264Encoder
 
 class MyApp():
     def __init__(self):
         print("init")
         self.cam = Picamera2()
+        self.path = "/home/me691kidcam/kid-camera/pics/default"
 
     def run(self):
         print("run")
@@ -15,12 +18,22 @@ class MyApp():
         self.cam.start()
         time.sleep(3)
         filename = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
-        self.cam.capture_file(filename)
+        filepath = os.path.join(self.path, filename)
+        self.cam.capture_file(filepath)
 
         self.cam.stop_preview()
         time.sleep(3)
+        
         self.cam.start_preview(Preview.QTGL)
+        encoder = H264Encoder()
+        self.cam.start_recording(encoder, '/home/me691kidcam/kid-camera/temp/test.h264')
         time.sleep(3)
+        self.cam.stop_recording()
+
+        self.cam.stop_preview()
+        self.cam.stop()
+
+        
 
 
 if __name__ == "__main__":
